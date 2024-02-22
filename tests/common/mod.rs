160adc7f6d8fd54a143aaa9e03b2a20a7bd959a1
John Lees <lees.john6@gmail.com>
2024-02-22 17:19:26 +0000

    Skeleton of phyml test

---
+++ tests/common/mod.rs
@@ -0,0 +1,78 @@
+use std::path::{Path, PathBuf};
+use std::fs::File;
+use std::io::{Write, BufReader, BufRead, Error};
+
+use assert_fs::{prelude::*, TempDir};
+use predicates::prelude::*;
+
+#[cfg(test)]
+
+// Creates correct path for input/output files
+static FILE_IN: &'static str = "tests/test_files_in";
+static SYM_IN: &'static str = "input";
+static SYM_TEST: &'static str = "correct";
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum TestDir {
+    Input,
+    Correct,
+}
+
+pub struct TestSetup {
+    wd: TempDir,
+}
+
+impl TestSetup {
+    pub fn setup() -> Self {
+        let wd = assert_fs::TempDir::new().unwrap();
+        wd.child(SYM_IN)
+            .symlink_to_dir(
+                Path::new(FILE_IN)
+                    .canonicalize()
+                    .expect("Could not link expected files"),
+            )
+            .unwrap();
+        Self { wd }
+    }
+
+    pub fn get_wd(&self) -> String {
+        self.wd.path().display().to_string()
+    }
+
+    pub fn create_file(&self, filename: &str) -> Result<(File, String), Error> {
+        let path = self.wd.with_file_name(filename);
+        let output = File::create(path.clone())?;
+        Ok((output, path.canonicalize().unwrap().to_str().unwrap().to_string()))
+    }
+
+    pub fn file_path(&self, name: &str, file_type: TestDir) -> PathBuf {
+        match file_type {
+            TestDir::Input => {
+                PathBuf::from(&format!("{}/{}/{}", self.wd.path().display(), SYM_IN, name))
+            }
+            TestDir::Correct => PathBuf::from(&format!(
+                "{}/{}/{}",
+                self.wd.path().display(),
+                SYM_TEST,
+                name
+            )),
+        }
+    }
+
+    pub fn file_string(&self, name: &str, file_type: TestDir) -> String {
+        self.file_path(name, file_type)
+            .to_str()
+            .expect("Could not unpack file path")
+            .to_owned()
+    }
+
+    pub fn file_check(&self, name_out: &str, name_correct: &str) -> bool {
+        let predicate_file = predicate::path::eq_file(self.wd.child(name_out).path());
+        predicate_file.eval(self.file_path(name_correct, TestDir::Correct).as_path())
+    }
+
+    pub fn file_exists(&self, name_out: &str) -> bool {
+        let predicate_fn = predicate::path::is_file();
+        predicate_fn.eval(self.wd.child(name_out).path())
+    }
+}
---
+++ tests/phyml.rs
@@ -0,0 +1,51 @@
+use std::io::Write;
+use regex::Regex;
+
+use snapbox::cmd::{cargo_bin, Command};
+use float_cmp::assert_approx_eq;
+
+pub mod common;
+use crate::common::*;
+
+#[cfg(test)]
+use pretty_assertions::assert_eq;
+
+#[test]
+fn jc69_likelihood() {
+    // Makes a temporary directory to work in
+    let sandbox = TestSetup::setup();
+    let input_alignment = sandbox.file_string("listeria0.phylip", TestDir::Input);
+
+    // Likelihood and treefrom program
+    // TODO when this can take input file use `.arg(sandbox.file_string("listeria0.aln", TestDir::Input))` to provide it
+    let output = Command::new(cargo_bin("bactrees"))
+        .current_dir(sandbox.get_wd())
+        .output()
+        .unwrap()
+        .stdout;
+    let output_string = String::from_utf8(output).unwrap();
+    let output_parts: Vec<&str> = output_string.split("\n").collect();
+    let likelihood: f64 = output_parts[0].parse().unwrap();
+    let mut output_tr_file = sandbox.create_file("tree.nwk").unwrap();
+    writeln!(output_tr_file.0, "{}", output_parts[1]);
+
+    // Parallel alignment algorithm used
+    // phyml -i <path_to_sequence> -u <path to newick tree> -m JC69 -o n -a 1
+    let mut phyml_likelihood: f64 = 0.0;
+    let phyml_out = Command::new("phyml")
+        .current_dir(sandbox.get_wd())
+        .args(&["-i", input_alignment.as_str(), "-u", output_tr_file.1.as_str(), "-m", "JC69", "-o", "n", "-a", "1"])
+        .output()
+        .unwrap()
+        .stdout;
+    let phyml_stdout = String::from_utf8(phyml_out).unwrap();
+
+    let re = Regex::new(r"^\. Log likelihood of the current tree: (.+)\.$").unwrap();
+    for phyml_outline in phyml_stdout.split("\n") {
+        if let Some(caps) = re.captures(phyml_outline) {
+            phyml_likelihood = caps.get(1).map_or("", |m| m.as_str()).parse().unwrap();
+        }
+    }
+
+    assert_approx_eq!(f64, likelihood, phyml_likelihood, ulps = 2)
+}
