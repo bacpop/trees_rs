1729e7292ad0ed7b05497bd6f8b2ae13f187bc97
Joel Hellewell <joel@ebi.ac.uk>
2024-04-24 16:40:26 +0100

    Progress on Newick parser, created vector of parents that can be turned into Tree struct

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,7 @@ use crate::tree::Tree;
 use crate::likelihoods::logse;
 use crate::node::Node;
 use std::collections::HashMap;
+use std::iter::SkipWhile;
 use std::thread::current;
 use std::time::Instant;
 extern crate nalgebra as na;
@@ -33,33 +34,47 @@ pub mod cli;
 use crate::cli::*;
 
 pub fn main() {
-    let args = cli_args();  
+    let args = cli_args(); 
 
-    // let nstr: String = String::from("(((((((((((((20:0.0005559237124905506,(18:0.0006861528539295744,(7:0.0003272353040145137,((19:0.00025082829155862905,25:0.0004406150965139729):0.00038079747821232975,(4:0.0011438935596238994,9:0.0006423351928969907):0.0002818357686905768):0.00011932188411570738):0.0000773158870672558):0.00010670953441236318):0.00009667944131959232,3:0.000765824159948059):0.00010205320788541966,11:0.001996574669283589):0.00015507020971381987,(15:0.0007456874819936607,16:0.0005219587294727793):0.0001577483845742265):0.00006260417952833508,22:0.0013071476159608165):0.00014168737395563787,1:0.0009298473062518015):0.00036278269951022337,(0:0.0017533936651583696,14:0.0019343044045621707):0.0005499810933448594):0.00028866410976663946,26:0.002536919918888372):0.00020634571061321078,(8:0.002071089023336213,17:0.0038061797752808968):0.00011298977239988664):0.0001818640161336798,((5:0.000657447421492366,27:0.000610198789974074):0.0021134939498703524,(13:0.0029180692656446667,(12:0.0016444697624472885,24:0.0019279877425944816):0.0006543882393971138):0.00020572241428982877):0.00005593218813022198):0.0008632913785652549,10:0.0019187896996885764):0.00044364187622614485,(2:0.0012526409296072239,23:0.0011674109286468862):0.0001283930204301479):0.0013985444615635147,6:0.0008410345056844609,21:0.0008875739644970391);");
-    // let mut new_nstr = nstr.clone();
+    // let left_start: &char = &'(';
+    // let left_end: &char = &',';
+    // let right_start: &char = &',';
+    // let right_end: &char = &')';
+    // let newick_end: &char = &';';
 
-    // let full_split: Vec<&str> = nstr.split(['(', ',',')',';']).filter(|c| !c.is_empty()).collect();
-    // // println!("{:?}", nstr);
-    // let mut name_dict: HashMap<usize, String> = HashMap::new();
-    // let mut branch_len: HashMap<usize, f64> = HashMap::new();
-    // let mut id: String;
-    // let mut bl: f64;
-    // let mut leaf_count = 0;
-    // for (i, x) in full_split.iter().enumerate() {
-    //     let bits: Vec<&str> = x.split(':').filter(|c| !c.is_empty()).collect();
+    let mut nstr: String = String::from("(((((((((((((20:0.0005559237124905506,(18:0.0006861528539295744,(7:0.0003272353040145137,((19:0.00025082829155862905,25:0.0004406150965139729):0.00038079747821232975,(4:0.0011438935596238994,9:0.0006423351928969907):0.0002818357686905768):0.00011932188411570738):0.0000773158870672558):0.00010670953441236318):0.00009667944131959232,3:0.000765824159948059):0.00010205320788541966,11:0.001996574669283589):0.00015507020971381987,(15:0.0007456874819936607,16:0.0005219587294727793):0.0001577483845742265):0.00006260417952833508,22:0.0013071476159608165):0.00014168737395563787,1:0.0009298473062518015):0.00036278269951022337,(0:0.0017533936651583696,14:0.0019343044045621707):0.0005499810933448594):0.00028866410976663946,26:0.002536919918888372):0.00020634571061321078,(8:0.002071089023336213,17:0.0038061797752808968):0.00011298977239988664):0.0001818640161336798,((5:0.000657447421492366,27:0.000610198789974074):0.0021134939498703524,(13:0.0029180692656446667,(12:0.0016444697624472885,24:0.0019279877425944816):0.0006543882393971138):0.00020572241428982877):0.00005593218813022198):0.0008632913785652549,10:0.0019187896996885764):0.00044364187622614485,(2:0.0012526409296072239,23:0.0011674109286468862):0.0001283930204301479):0.0013985444615635147,6:0.0008410345056844609,21:0.0008875739644970391);");
+    let mut new_nstr = nstr.clone();
+
+    let full_split: Vec<&str> = nstr.split(['(', ',',')',';']).filter(|c| !c.is_empty()).collect();
+    // println!("{:?}", nstr);
+    let mut name_dict: HashMap<usize, String> = HashMap::new();
+    let mut branch_len: HashMap<usize, f64> = HashMap::new();
+    let mut id: String;
+    let mut bl: f64;
+    let mut leaf_count = 0;
+    for (i, x) in full_split.iter().enumerate() {
+        let bits: Vec<&str> = x.split(':').filter(|c| !c.is_empty()).collect();
         
-    //     bl = bits.last().unwrap().parse().unwrap();
-    //     branch_len.insert(i, bl);
+        bl = bits.last().unwrap().parse().unwrap();
+        branch_len.insert(i, bl);
 
-    //     if bits.len().eq(&2) {
-    //         name_dict.insert(i, bits.first().unwrap().to_string());
-    //     }
+        if bits.len().eq(&2) {
+            name_dict.insert(i, bits.first().unwrap().to_string());
+        }
 
-    //     new_nstr = new_nstr.replace(x, &format!("{}:{}", i.to_string(), bl));
+        // new_nstr = new_nstr.replace(x, &format!("{}:{}", i.to_string(), bl));
+        new_nstr = new_nstr.replace(x, &format!("{}", i.to_string()));
         
-    // };
+    };
 
     // println!("{:?}", new_nstr);
+
+    nstr = String::from("(((((((((((((0,(1,(2,((3,4)5,(6,7)8)9)10)11)12,13)14,15)16,(17,18)19)20,21)22,23)24,(25,26)27)28,29)30,(31,32)33)34,((35,36)37,(38,(39,40)41)42)43)44,45)46,(47,48)49)50,51);");
+    println!("{:?}", nstr);
+    // // let nnstr: = nstr.chars().filter(|c| c.ne(newick_end)).collect();
+    // nstr = nstr.chars().filter(|c| c.ne(&';')).collect();
+    // let mut full_split: Vec<&str> = nstr.split([')'],   ).filter(|c| !c.is_empty()).collect();
+    // println!("{:?}", full_split);
 
     // let rooted_nstr: String = String::from("(((((((((((((0:0.0005559237124905506,(1:0.0006861528539295744,(2:0.0003272353040145137,((3:0.00025082829155862905,4:0.0004406150965139729)5:0.00038079747821232975,(6:0.0011438935596238993,7:0.0006423351928969907)8:0.0002818357686905768)9:0.00011932188411570738)10:0.0000773158870672558)11:0.00010670953441236318)12:0.00009667944131959232,13:0.000765824159948059)14:0.00010205320788541966,15:0.001996574669283589)16:0.00015507020971381987,(17:0.0007456874819936607,18:0.0005219587294727793)19:0.0001577483845742265)20:0.00006260417952833508,21:0.0013071476159608165)22:0.00014168737395563787,23:0.0009298473062518015)24:0.00036278269951022337,(25:0.0017533936651583696,26:0.0019343044045621706)27:0.0005499810933448594)28:0.00028866410976663946,29:0.002536919918888372)30:0.00020634571061321078,(31:0.002071089023336213,32:0.0038061797752808966)33:0.00011298977239988664)34:0.0001818640161336798,((35:0.000657447421492366,36:0.000610198789974074)37:0.0021134939498703522,(38:0.0029180692656446666,(39:0.0016444697624472884,40:0.0019279877425944815)41:0.0006543882393971138)42:0.00020572241428982877)43:0.00005593218813022198)44:0.0008632913785652549,45:0.0019187896996885763)46:0.00044364187622614485,(47:0.0012526409296072238,48:0.0011674109286468862)49:0.0001283930204301479)50:0.0013985444615635147,(51:0.0008410345056844609,52:0.0008875739644970391)53:0.0001);");
     // let y = newick_to_vec(&rooted_nstr, name_dict.len());
@@ -69,14 +84,49 @@ pub fn main() {
     // println!("{:?}", branch_len);
     // println!("leaf count: {}", name_dict.len());
 
-    // let left_start: &char = &'(';
-    // let left_end: &char = &',';
-    // let right_start: &char = &',';
-    // let right_end: &char = &')';
-    // let newick_end: &char = &';';
-    // let mut leaf_idx : usize = 0;
+    let left_start: &char = &'(';
+    let left_end: &char = &',';
+    let right_start: &char = &',';
+    let right_end: &char = &')';
+    let newick_end: &char = &';';
+    let mut leaf_idx : usize = 0;
+    let mut current_parent: Option<usize> = Some(52);
+    let mut parent_vector: Vec<Option<usize>> = vec![None; 53];
+    let mut idx: Option<usize> = Some(52);
+    
+    for i in (0..nstr.len()).rev() {
 
+        let ch: char = nstr.chars().nth(i).unwrap();
+        println!("i: {i} and ch: {ch}");
+        if ch.eq(right_end) || ch.eq(left_end) {
 
+            if ch.eq(right_end) {
+                println!("current_parent = {:?}", idx);
+                current_parent = idx
+            }
+
+            let mut j = i - 1;
+            let mut jch: char = nstr.chars().nth(j).unwrap();
+            while j > 0 && jch.ne(left_start) && jch.ne(left_end) && jch.ne(right_end) {
+                j -= 1;
+                jch = nstr.chars().nth(j).unwrap();
+            }
+
+            if j != (i - 1) {
+                let mut leaf: &str = &nstr[(j + 1)..i];
+                idx = Some(leaf.parse().unwrap());
+                println!("leaf: {leaf}");
+                println!("Setting parent_vector[{:?}] to {:?}", idx, current_parent);
+                parent_vector[idx.unwrap()] = current_parent;
+                // println!("{:?}", leaf);
+            }
+        } else if ch.eq(left_start) {
+            current_parent = parent_vector[current_parent.unwrap()];
+            println!("current_parent = {:?}", current_parent);
+        }
+    }
+
+    println!("{:?}", parent_vector);
     // for i in 0..nstr.len() {
     //     // println!("i: {i}");
     //     let ch: char = nstr.chars().nth(i).unwrap();
@@ -96,14 +146,14 @@ pub fn main() {
 
     //         if j != (i + 1) {
     //             let mut leaf: &str = &nstr[(i + 1)..(j)];
-    //             let spl: Vec<&str> = leaf.split(':').collect();
-    //             name_dict.insert(leaf_idx, spl[0].to_string());
-    //             let bl: f64 = spl[1].parse().unwrap();
-    //             branch_len.insert(leaf_idx, bl);
+    //             // let spl: Vec<&str> = leaf.split(':').collect();
+    //             // name_dict.insert(leaf_idx, spl[0].to_string());
+    //             // let bl: f64 = spl[1].parse().unwrap();
+    //             // branch_len.insert(leaf_idx, bl);
     //             // println!("{:?}", leaf);
     //             // println!("{:?}", &leaf_idx.to_string());
     //             // println!("{:?}", new_nstr);
-    //             new_nstr = new_nstr.replace(leaf, &leaf_idx.to_string());
+    //             // new_nstr = new_nstr.replace(leaf, &leaf_idx.to_string());
     //             // println!("{:?}", new_nstr);
     //             leaf_idx += 1;
     //         }
@@ -127,9 +177,9 @@ pub fn main() {
 
     tr.update_likelihood_postorder(&q);
 
-    println!("{}", tr.get_tree_likelihood());
-    println!("{:?}", tr.newick());
-    println!("{:?}", tr.tree_vec);
+    // println!("{}", tr.get_tree_likelihood());
+    // println!("{:?}", tr.newick());
+    // println!("{:?}", tr.tree_vec);
 
     if !args.no_optimise {
         // tr.hillclimb(&q, 100);
