b5cb1353386496bacb5b101ebab341009ff586e1
jhellewell14 <joel@ebi.ac.uk>
2024-07-26 09:52:11 +0100

    Fixed main and checked that cargo test and run are working

--- src/lib.rs
+++ src/lib.rs
@@ -42,28 +42,20 @@ pub fn main() {
 
     // let mut tr = vector_to_tree(&random_vector(4));
     // tr.add_genetic_data(&String::from("/Users/joel/Downloads/listeria0.aln"));
-    // println!("{:?}", tr.mutation_lists);
     let mut tr = vector_to_tree(&random_vector(27));
     tr.add_genetic_data(&args.alignment);
 
-    // println!("{:?}", tr.nodes.get(4));
     tr.initialise_likelihood(&q);
     println!("{}", tr.get_tree_likelihood());
-    // tr.update(&random_vector(27));
-    // let y: ChangeOrder = tr.changeiter();
-    // println!("{:?}", y);
-    // println!("{:?}", tr.mutation_lists);
     println!("{:?}", tr.newick());
-    // println!("{:?}", tr.mutation_lists.get(2));
-    // println!("{:?}", tr.mutation_lists.get(3));
     println!("{:?}", tr.tree_vec);
 
     if !args.no_optimise {
-        // let start = Instant::now();
-        // tr.hillclimb(&q, 50);
-        // let end = Instant::now();
+        let start = Instant::now();
+        tr.hillclimb(&q, 50);
+        let end = Instant::now();
 
-        // eprintln!("Done in {}s", end.duration_since(start).as_secs());
-        // eprintln!("Done in {}s", end.duration_since(start).as_millis());
+        eprintln!("Done in {}s", end.duration_since(start).as_secs());
+        eprintln!("Done in {}s", end.duration_since(start).as_millis());
     }
 }
