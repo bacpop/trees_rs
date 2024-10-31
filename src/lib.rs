d30e1ad1221fcb3cf24b853a9348b19fed60077d
jhellewell14 <joel@ebi.ac.uk>
2024-10-31 14:16:52 +0000

    Fixed optimisation code in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -43,19 +43,29 @@ pub fn main() {
     println!("{:?}", t.get_newick());
     println!("{:?}", t.tree_vec);
 
-    let new_v = random_vector(27);
+    // let new_v = random_vector(27);
+    // let mut t2: Topology = Topology::from_vec(&new_v);
 
-    let mv = ExactMove{target_vector: new_v};
-    t.apply_move(mv, hillclimb_accept, &mut gen_data, &p.get_matrix());
-    println!("{:?}", likelihood(&t, &gen_data));
+    // let mv = ExactMove{target_vector: new_v};
+    // t.apply_move(mv, hillclimb_accept, &mut gen_data, &p.get_matrix());
+    // println!("updated ll: {:?}, initialised ll: {:?}", likelihood(&t, &gen_data), likelihood(&t2, &gen_data));
+
+    
 
     if !args.no_optimise {
-        // let start = Instant::now();
-        // tr.hillclimb(0);
-        // let end = Instant::now();
-        println!("Optimisation currently broken");
-        // eprintln!("Done in {}s", end.duration_since(start).as_secs());
-        // eprintln!("Done in {}ms", end.duration_since(start).as_millis());
+        let start = Instant::now();
+        for i in 0..10 {
+            println!{"Step {}", i};
+            // let new_v = random_vector(27);
+            // let mv = ExactMove{target_vector: new_v};
+            let mv = PeturbVec{n: 10};
+            t.apply_move(mv, hillclimb_accept, &mut gen_data, &p.get_matrix());
+            
+        }
+        let end = Instant::now();
+        println!("New likelihood: {:?}", likelihood(&t, &gen_data));
+        eprintln!("Done in {}s", end.duration_since(start).as_secs());
+        eprintln!("Done in {}ms", end.duration_since(start).as_millis());
     }
 
 }
